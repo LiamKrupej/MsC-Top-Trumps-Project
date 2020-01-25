6b1f9eeef03313b8b7496b8676d12618f16cd48a
BenScott88 <60007760+BenScott88@users.noreply.github.com>
2020-01-25 15:59:34 +0000

    Add files via upload

---
+++ TTController.java
@@ -0,0 +1,15 @@
+public class TTController {
+
+	private TTModel model;
+	private TTView	view;
+
+	public TTController(TTModel tModel, TTView tView) {
+		model = tModel;
+		view = tView;
+	}
+
+	public void startGame() {
+
+	}
+	
+}
