aaff96b64230b59558f7a2e22e52a7a5ce5a7ca3
nabs95 <60003465+nabs95@users.noreply.github.com>
2020-02-09 19:04:59 +0000

    Add files via upload

---
+++ Selectionscreen.java
@@ -0,0 +1,86 @@
+<html>
+
+	<head>
+		<!-- Web page title -->
+    	<title>Top Trumps</title>
+    	
+    	<!-- Import JQuery, as it provides functions you will probably find useful (see https://jquery.com/) -->
+    	<script src="https://code.jquery.com/jquery-2.1.1.js"></script>
+    	<script src="https://code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+    	<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.1/themes/flick/jquery-ui.css">
+
+		<!-- Optional Styling of the Website, for the demo I used Bootstrap (see https://getbootstrap.com/docs/4.0/getting-started/introduction/) -->
+		<link rel="stylesheet" href="http://dcs.gla.ac.uk/~richardm/TREC_IS/bootstrap.min.css">
+    	<script src="http://dcs.gla.ac.uk/~richardm/vex.combined.min.js"></script>
+    	<script>vex.defaultOptions.className = 'vex-theme-os';</script>
+    	<link rel="stylesheet" href="http://dcs.gla.ac.uk/~richardm/assets/stylesheets/vex.css"/>
+    	<link rel="stylesheet" href="http://dcs.gla.ac.uk/~richardm/assets/stylesheets/vex-theme-os.css"/>
+    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link href="https://unpkg.com/nes.css/css/nes.min.css" rel="stylesheet" />
+		<link href='http://fonts.googleapis.com/css?family=Press+Start+2P' rel='stylesheet' type='text/css'>
+		
+		<style>
+			
+			body{
+				background-image: url();
+			}
+			
+			h1 {
+			font-weight: 400;
+			font-size: 50px;
+			color: white;
+ 			padding-top: 25%; 
+			}
+			
+			.nes-btn {
+			position: absolute;
+			left: 45%;
+			font-size: 15px;
+			padding: 10px 10px;
+			}
+		
+		</style>
+
+	</head>
+
+    <body>
+    	<div class="container">
+      		<div class="row">
+      			<div class="col-lg">
+      				<h1 align="center">Top Trumps</h1>
+      			</div>
+        	</div>
+        	<div class="row">
+      			<div class="col-lg">
+      				<button class="nes-btn" style="left: 35%;" onclick="window.location.href ='http://localhost:7777/toptrumps/game';">Play</button>
+        			<button class="nes-btn" onclick="window.location.href= 'http://localhost:7777/toptrumps/stats';">View Statistics</button>
+      			</div>
+        	</div>
+    	</div>
+		
+		<script type="text/javascript">
+		
+			// This is a reusable method for creating a CORS request. Do not edit this.
+			function createCORSRequest(method, url) {
+  				var xhr = new XMLHttpRequest();
+  				if ("withCredentials" in xhr) {
+    				// Check if the XMLHttpRequest object has a "withCredentials" property.
+    				// "withCredentials" only exists on XMLHTTPRequest2 objects.
+    				xhr.open(method, url, true);
+  				} else if (typeof XDomainRequest != "undefined") {
+    				// Otherwise, check if XDomainRequest.
+    				// XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    				xhr = new XDomainRequest();
+    				xhr.open(method, url);
+ 				 } else {
+    				// Otherwise, CORS is not supported by the browser.
+    				xhr = null;
+  				 }
+  				 return xhr;
+			}
+		
+		</script>
+		
+	</body>
+</html>
